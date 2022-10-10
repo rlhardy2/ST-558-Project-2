bc0e5e392bc800f93dece0f5bb27f5b354f9210e
Matthew Sookoo <mbsookoo@ncsu.edu>
2022-10-10 17:45:23 -0400

    R script render code

--- R script render code.R
+++ R script render code.R
@@ -1,4 +1,4 @@
 rmarkdown:: render("ST558 Project 2.Rmd",
                    output_format = "github_document",
                    output_file = "README.md",
-                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "1", df_print = "tibble"))
+                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "6", df_print = "tibble"))
